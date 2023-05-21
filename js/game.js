99b44825718460b5b97c1bc67ab4303bea07a833
noclipst <noclipst@gmail.com>
2023-05-21 16:24:00 +0300

    Implement complete game logic

--- js/game.js
+++ js/game.js
@@ -39,7 +39,7 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     let result;
 
-    if (playerSelection === "ROCK" || playerSelection === "PAPER" || playerSelection === "SCISSORS") {
+    if (playerSelection === "ROCK" || playerSelection === "PAPER" || playerSelection === "SCISSORS") { // to make sure we're passing through valid inputs only
         if (playerSelection === computerSelection) {
             result = `Draw! You selected ${playerSelection} and so did your opponent!`;
         } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
@@ -65,6 +65,36 @@ function playRound(playerSelection, computerSelection) {
 //      track how many times player and PC won
 //      do this five times in a row
 //      in the end, compare how many times player and PC won and show the general reuslt
-//          if playerWon > aiWon then player wins
-//          else if aiWon > playerWon then PC wins
+//          if gamesWonByPlayer > gamesWonByAi then player wins
+//          else if gamesWonByAi > gamesWonByPlayer then PC wins
 //          else draw
+function game() {
+    let playerInput = "";
+    let playerChoice = "";
+    let computerChoice = "";
+    let gamesWonByPlayer = 0;
+    let gamesWonByAi = 0;
+    let roundResult = "";
+
+    for (let i = 0; i < 5; i++) { // play the game five times in a row
+        computerChoice = getComputerChoice();
+        playerInput = prompt("Please type in your selection. Valid inputs are \"Rock\", \"Paper\" or \"Scissors\"");
+        playerChoice = playerInput.toUpperCase(); // to make input case-insensitive to allow for proper comparisons
+        roundResult = playRound(playerChoice, computerChoice);
+        if (roundResult.includes("You win this round")) {
+            gamesWonByPlayer++;
+        } else if (roundResult.includes("You lose this round")) {
+            gamesWonByAi++;
+        }
+        console.log(roundResult);
+    }
+
+    if (gamesWonByPlayer === gamesWonByAi) {
+        console.log(`You won ${gamesWonByPlayer} rounds and so did the AI. It's a draw!`);
+    } else if (gamesWonByPlayer > gamesWonByAi) {
+        console.log(`You won ${gamesWonByPlayer} rounds while the AI won ${gamesWonByAi} rounds. That makes you the winner!`)
+    } else {
+        console.log(`You won ${gamesWonByPlayer} rounds while the AI won ${gamesWonByAi} rounds. You lose, shit happens!`)
+    }
+
+}
